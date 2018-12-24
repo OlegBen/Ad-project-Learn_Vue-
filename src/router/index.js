362da0539b12eef798df09333f0a51fc322595ca
Oleg <ben.oleg.i.1.17@gmail.com>
2018-12-24 14:32:55 +0300

    Lesson 91. Created new routes and switch them

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,52 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Home from '@/Components/Home'
+import Ad from '@/Components/Ads/Ad'
+import AdList from '@/Components/Ads/AdList'
+import NewAd from '@/Components/Ads/NewAd'
+import Login from '@/Components/Auth/Login'
+import Registration from '@/Components/Auth/Registration'
+import Orders from '@/Components/User/Orders'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: []
+  routes: [
+    {
+      path: '',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/ad/:id',
+      name: 'ad',
+      component: Ad
+    },
+    {
+      path: '/list',
+      name: 'list',
+      component: AdList
+    },
+    {
+      path: '/new',
+      name: 'newAd',
+      component: NewAd
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/registration',
+      name: 'reg',
+      component: Registration
+    },
+    {
+      path: '/orders',
+      name: 'orders',
+      component: Orders
+    }
+  ],
+  mode: 'history'
 })
