db7855603122460a86c6f3eff0ce0fbe734237e9
Oleg <ben.oleg.i.1.17@gmail.com>
2018-12-27 15:57:24 +0300

    Connected firebase, and creted new users.js file

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import store from './store'
+import * as fb from 'firebase'
 
 Vue.use(Vuetify)
 
@@ -15,6 +16,16 @@ new Vue(
     router,
     store,
     components: { App },
-    template: '<App/>'
+    template: '<App/>',
+    created () {
+      fb.initializeApp({
+        apiKey: 'AIzaSyDUbMaMm5YctF6oAFuov5TUCo3DGSsnx7U',
+        authDomain: 'ad-project-bdd33.firebaseapp.com',
+        databaseURL: 'https://ad-project-bdd33.firebaseio.com',
+        projectId: 'ad-project-bdd33',
+        storageBucket: 'ad-project-bdd33.appspot.com',
+        messagingSenderId: '996281839257'
+      })
+    }
   }
 )
